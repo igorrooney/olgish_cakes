2dad4ad38f9fb11a6b81ef8246004b8eb9f96f63
Igor Ieromenko <igorrooney@gmail.com>
2025-07-15 22:11:32 +0100

    Add H4, H5, and H6 options to rich text formatting in Sanity schema for cake. Enhance text styling capabilities for improved content management.

--- sanity/schemas/cake.ts
+++ sanity/schemas/cake.ts
@@ -31,6 +31,9 @@ export default {
             { title: "H1", value: "h1" },
             { title: "H2", value: "h2" },
             { title: "H3", value: "h3" },
+            { title: "H4", value: "h4" },
+            { title: "H5", value: "h5" },
+            { title: "H6", value: "h6" },
             { title: "Quote", value: "blockquote" },
           ],
           marks: {
@@ -76,6 +79,9 @@ export default {
             { title: "H1", value: "h1" },
             { title: "H2", value: "h2" },
             { title: "H3", value: "h3" },
+            { title: "H4", value: "h4" },
+            { title: "H5", value: "h5" },
+            { title: "H6", value: "h6" },
             { title: "Quote", value: "blockquote" },
           ],
           marks: {
